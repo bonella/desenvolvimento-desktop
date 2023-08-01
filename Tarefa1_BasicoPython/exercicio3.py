cd6107d5e36b4d64a5785cbd57c2ef8f0a9a702f
Gustavo S <gustavobonellasantos@gmail.com>
2023-08-01 20:30:40 -0300

    Resolução do exercício

---
+++ Tarefa1_BasicoPython/exercicio3.py
@@ -0,0 +1,22 @@
+numeros = []
+valida = True
+
+numeros.append(input("Digite o primeiro número: "))
+numeros.append(input("Digite o segundo número: "))
+
+def verifica_inteiros(numero) -> bool:
+    while True:
+        try:
+            int(numero)
+            return True
+        except ValueError as e:
+            print(f'O valor {numero} não é numérico.')
+            print(f'Erro: {e}')
+            return False
+
+for n in numeros:
+    if verifica_inteiros(n) is False:
+        valida = False
+
+if valida:
+    print(f'O valor da soma do números {numeros[0]} e {numeros[1]} é: {int(numeros[0]) + int(numeros[1])}')
---
+++ Tarefa1_BasicoPython/exercicio7.py
@@ -0,0 +1,3 @@
+celsius = float(input('Escreva a temperatura em graus Celsius: '))
+print(f'A temperatura {celsius} graus Celsius convertida para Fahrenheit'
+      f'é {(celsius * 9/5) + 32}.')
