54d7cd909607fdeafe53325acb1ad3fc903732f7
bonella <gustavobonellasantos@gmail.com>
2023-08-12 00:22:41 -0300

    Update exercicio

--- POO/exercicioAula.py
+++ POO/exercicioAula.py
@@ -2,7 +2,6 @@ class Animal:
     def __init__(self, nome, especie):
         self.nome = nome
         self.especie = especie
-
     def emitir_som(self):
         pass
 
@@ -15,7 +14,7 @@ class Cachorro(Animal):
         return self.__raca
 
     def emitir_som(self):
-        return 'O cachorro late.'
+        return 'late'
 
 class Gato(Animal):
     def __init__(self, nome, especie, pelagem):
@@ -26,12 +25,11 @@ class Gato(Animal):
         return self.__pelagem
 
     def emitir_som(self):
-        return 'O gato mia.'
+        return 'mia'
+
 
 cachorro = Cachorro('Bob', 'Canino', 'PitBull')
-print(f'Meu cachorro {cachorro.nome} é da espécie {cachorro.especie} e raça {cachorro.obter_raca()}')
-print(cachorro.emitir_som())
+print(f'Meu cachorro {cachorro.nome} é da espécie {cachorro.especie} e da raça {cachorro.obter_raca()}. As vezes ele {cachorro.emitir_som()} a noite.\n')
 
 gato = Gato('Mimi', 'Felino', 'Fofinho')
-print(f'Meu gato {gato.nome} é da espécie {gato.especie} e raça {gato.obter_pelagem()}')
-print(gato.emitir_som())
+print(f'Meu gato {gato.nome} é da espécie {gato.especie} e pelagem {gato.obter_pelagem()}. Quando o {gato.nome} tem fome ele {gato.emitir_som()}.')
